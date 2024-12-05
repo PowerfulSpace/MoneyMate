80076fd0b2c69b131ccf9490c63f9649405d85ca
Alexander <sany199574@gmail.com>
2024-12-05 09:37:56 +0300

    Добавлены сущности, и реализованы  - Currency  - ExchangeRate

---
+++ src/MoneyMate/PS.MoneyMate.Domain/Entities/Currency.cs
@@ -0,0 +1,8 @@
+﻿namespace PS.MoneyMate.Domain.Entities
+{
+    public class Currency
+    {
+        public string Code { get; set; } = null!;// Код валюты (например, USD)
+        public string Name { get; set; } = null!; // Название валюты
+    }
+}
---
+++ src/MoneyMate/PS.MoneyMate.Domain/Entities/ExchangeRate.cs
@@ -0,0 +1,9 @@
+﻿namespace PS.MoneyMate.Domain.Entities
+{
+    public class ExchangeRate
+    {
+        public string FromCurrency { get; set; } = null!;
+        public string ToCurrency { get; set; } = null!;
+        public decimal Rate { get; set; }
+    }
+}
