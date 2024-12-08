03ebb0ef413068874af5c3429839b78dc9c857db
Alexander <sany199574@gmail.com>
2024-12-08 13:49:36 +0300

    Добавлен контроллер  - ConversionRequestController

---
+++ src/MoneyMate/PS.MoneyMate.Web/Controllers/ConversionRequestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PS.MoneyMate.Web.Controllers
+{
+    public class ConversionRequestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
