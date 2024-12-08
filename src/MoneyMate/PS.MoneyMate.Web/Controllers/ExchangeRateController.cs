27cc7d78e77488314dedef08531cdf088fe71593
Alexander <sany199574@gmail.com>
2024-12-08 13:50:32 +0300

    Добавлен контроллер  - ExchangeRateController

---
+++ src/MoneyMate/PS.MoneyMate.Web/Controllers/ExchangeRateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PS.MoneyMate.Web.Controllers
+{
+    public class ExchangeRateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
