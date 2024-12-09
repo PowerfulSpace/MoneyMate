d2e0ea5a97ded5c0a3aa3065755f414964720d1e
Alexander <sany199574@gmail.com>
2024-12-09 22:26:12 +0300

    Удалён лишний код

--- src/MoneyMate/PS.MoneyMate.Web/Program.cs
+++ src/MoneyMate/PS.MoneyMate.Web/Program.cs
@@ -10,7 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
         .AddInfrastructure(builder.Configuration);
 }
 
-
 var app = builder.Build();
 {
     if (!app.Environment.IsDevelopment())
